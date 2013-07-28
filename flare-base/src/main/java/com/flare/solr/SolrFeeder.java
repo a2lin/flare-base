52c143f21269d331b7d5faf099a6d0178d4299c6
Alexander Lin <lin.alexander.2011@gmail.com>
2013-07-28 12:04:15 -0700

    Added DateParser Utility

--- flare-base/src/main/java/com/flare/solr/SolrFeeder.java
+++ flare-base/src/main/java/com/flare/solr/SolrFeeder.java
@@ -1,5 +1,41 @@
 package com.flare.solr;
 
-public class SolrFeeder {
+import java.util.ArrayList;
 
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.impl.ConcurrentUpdateSolrServer;
+import org.apache.solr.common.SolrInputDocument;
+
+
+public class SolrFeeder {
+	
+	private SolrServer sserv;
+	private ArrayList<ArrayList<String>> eventList;
+	public SolrFeeder(String uri, ArrayList<ArrayList<String>> arr)
+	{
+		sserv = new ConcurrentUpdateSolrServer(uri,100,4);
+	}
+	public void addDocsToServer()
+	{
+		int id = 0;
+		for(int i = 0; i < eventList.size(); i++)
+		{
+			SolrInputDocument sid = new SolrInputDocument();
+			sid.addField("id",eventList.get(i).get(0) + id);
+			String quarter = eventList.get(i).get(0).substring(0, 2);
+			String year = eventList.get(i).get(0).substring(2, 4);
+			sid.addField("quarter", quarter);
+			sid.addField("year",year);
+			sid.addField("subjcode", eventList.get(i).get(1));
+			sid.addField("sectid", eventList.get(i).get(2) );
+			sid.addField("doctype", eventList.get(i).get(3) );
+			sid.addField("sectcode", eventList.get(i).get(4));
+			sid.addField("days", eventList.get(i).get(5));
+			
+			String splitter = eventList.get(i).get(6);
+			String[] split = splitter.split(" - ");
+			
+
+		}
+	}
 }
---
+++ flare-base/src/main/java/com/flare/util/DateParser.java
@@ -0,0 +1,35 @@
+package com.flare.util;
+
+public class DateParser {
+	
+	private String date;
+	
+	public DateParser(String date)
+	{
+		this.date = date;
+	}
+	
+	public int[] getDateInt()
+	{
+		String[] split = date.split(" - ");
+		int[] result = new int[split.length];
+    	for(int i = 0; i < split.length; i++)
+    	{
+    		String[] split2 = split[i].split(":");
+    		String part1 = split2[0];
+    		String part2 = split2[1].substring(0,split2[1].length()-1);
+    		int time = Integer.parseInt(part1+part2);
+    		String hourmark = split2[1].substring(split2[1].length()-1,split2[1].length());
+    		if(hourmark.equals("p") && time != 1200)
+    		{
+    			time += 1200;
+    		}
+    		if(hourmark.equals("a") && time==1200)
+    		{
+    			time -= 1200;
+    		}
+    		result[i] = time;
+    	}
+    	return result;
+	}
+}
